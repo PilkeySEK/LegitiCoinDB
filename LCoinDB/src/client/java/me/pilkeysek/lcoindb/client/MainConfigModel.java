a2b088d49e55e367b43a052e29d520579be3f6d0
PilkeySEK <PilkeySEK@gmx.de>
2024-11-02 12:16:25 +0100

    Changed api url from lcapi.skye.host to lcapi.minceraft.host (as it has been moved), also added lcauth.minceraft.host as a trusted auth server

--- LCoinDB/src/client/java/me/pilkeysek/lcoindb/client/MainConfigModel.java
+++ LCoinDB/src/client/java/me/pilkeysek/lcoindb/client/MainConfigModel.java
@@ -10,8 +10,8 @@ import java.util.List;
 @Config(name = "lcoindb-config", wrapperName = "MainConfig")
 public class MainConfigModel {
     public boolean authenticationEnabled = true;
-    public List<String> trustedAuthServers = List.of("localhost","localhost:25565","lcauth.skye.host");
-    public String apiUrl = "http://lcapi.skye.host";
+    public List<String> trustedAuthServers = List.of("localhost","localhost:25565","lcauth.skye.host","lcauth.minceraft.host");
+    public String apiUrl = "http://lcapi.minceraft.host";
     @ExcludeFromScreen
     public String secret = "";
 }
